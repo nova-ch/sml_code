f183b6fb949cade6a1cbe35473890ee001a98545
Tasnuva Chowdhury <tachowdh@cern.ch>
2025-02-06 07:34:19 -0700

    Update base_model.py

--- src/scout_ml_package/model/base_model.py
+++ src/scout_ml_package/model/base_model.py
@@ -140,7 +140,7 @@ class MultiOutputModel:
         inputs = Input(shape=(self.input_shape,))
         x = tf.keras.layers.Reshape((self.input_shape, 1))(inputs)
         x = self._add_conv_block(
-            x, filters=512, kernel_size=3, activation="relu", pool_size=2
+            x, filters=512, kernel_size=3, activation="swish", pool_size=2
         )
         x = Flatten()(x)
         x = self._add_dense_block(
