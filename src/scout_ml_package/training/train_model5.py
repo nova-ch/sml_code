664f553509e9450f23df13714659d4c67dfed397
Tasnuva Chowdhury <tachowdh@cern.ch>
2025-02-06 21:24:33 -0700

    Update train_model5.py

--- src/scout_ml_package/training/train_model5.py
+++ src/scout_ml_package/training/train_model5.py
@@ -1,7 +1,9 @@
 # src/scout_ml_package/train_model.py
+import pandas as pd
+import numpy as np
+import joblib
+import re
 
-# from scout_ml_package.model import MultiOutputModel, TrainedModel, PredictionVisualizer
-# from scout_ml_package.model.base_model import ModelTrainer, ModelPipeline
 from keras.layers import TFSMLayer
 from scout_ml_package.data import (
     HistoricalDataProcessor,
@@ -18,24 +20,123 @@ import joblib
 # processor = HistoricalDataProcessor(task_data_path='path/to/task_data.parquet',
 #                                      additional_data_path='path/to/additional_data.parquet')
 
-task_train_data_path = "/Users/tasnuvachowdhury/Desktop/projects/draft_projects/SML/local_data/training_historial.parquet"
-processor = HistoricalDataProcessor(task_train_data_path)
 
-task_new_data_path = "/Users/tasnuvachowdhury/Desktop/projects/draft_projects/SML/local_data/new_historial.parquet"
-new_preprocessor = HistoricalDataProcessor(task_new_data_path)
-# Filter the data
-training_data = processor.filtered_data()
-future_data = new_preprocessor.filtered_data()
+def preprocess_data(df):
+    # Convert PROCESSINGTYPE to 'P'
+    def convert_processingtype(processingtype):
+        if processingtype is not None and re.search(r"-.*-", processingtype):
+            return "-".join(processingtype.split("-")[-2:])
+        return processingtype
+
+    # Convert TRANSHOME to 'F'
+    def convert_transhome(transhome):
+        # Check if transhome is None
+        if transhome is None:
+            return None  # or handle as needed
+
+        if "AnalysisBase" in transhome:
+            return "AnalysisBase"
+        elif "AnalysisTransforms-" in transhome:
+            # Extract the part after 'AnalysisTransforms-'
+            part_after_dash = transhome.split("-")[1]
+            return part_after_dash.split("_")[
+                0
+            ]  # Assuming you want the first part before any underscore
+        elif "/" in transhome:
+            # Handle cases like AthGeneration/2022-11-09T1600
+            return transhome.split("/")[0]
+        else:
+            # For all other cases, split by '-', return the first segment
+            return transhome.split("-")[0]
+
+    # Convert CORECOUNT to 'Core'
+    def convert_corecount(corecount):
+        return "S" if corecount == 1 else "M"
+
+    # Apply transformations
+    df["P"] = df["PROCESSINGTYPE"].apply(convert_processingtype)
+    df["F"] = df["TRANSHOME"].apply(convert_transhome)
+
+    df["CTIME"] = np.where(
+        df["CPUTIMEUNIT"] == "mHS06sPerEvent",
+        df["CPUTIME"] / 1000,
+        np.where(df["CPUTIMEUNIT"] == "HS06sPerEvent", df["CPUTIME"], None),
+    )
+    df['CTIME'] = df['CTIME'].astype('float64')
+
+    KEEP_F_TAG = [
+        "Athena",
+        "AnalysisBase",
+        "AtlasOffline",
+        "AthAnalysis",
+        "AthSimulation",
+        "MCProd",
+        "AthGeneration",
+        "AthDerivation",
+    ]
+    KEEP_P_TAG = [
+        "jedi-run",
+        "deriv",
+        "athena-trf",
+        "jedi-athena",
+        "simul",
+        "pile",
+        "merge",
+        "evgen",
+        "reprocessing",
+        "recon",
+        "eventIndex",
+    ]
+
+    df["P"] = df["P"].apply(lambda x: x if x in KEEP_P_TAG else "others")
+    df["F"] = df["F"].apply(lambda x: x if x in KEEP_F_TAG else "others")
+    return df
+
+
+base_path = "/data/model-data/"
+categorical_features = ["PRODSOURCELABEL", "P", "F", "CORE"]
+data = pd.read_parquet("/data/model-data/merged_files/c_task.parquet")
+dataset = pd.read_parquet("/data/model-data/merged_files/c_data.parquet")
+ceff = pd.read_parquet("/data/model-data/merged_files/c_eff.parquet")
+df_ = pd.merge(data, dataset, on="JEDITASKID", how="right")
+df_ = pd.merge(df_, ceff, on="JEDITASKID", how="left")
+
+# task_train_data_path = "/Users/tasnuvachowdhury/Desktop/projects/draft_projects/SML/local_data/training_historial.parquet"
+# processor = HistoricalDataProcessor(task_train_data_path)
+
+# task_new_data_path = "/Users/tasnuvachowdhury/Desktop/projects/draft_projects/SML/local_data/new_historial.parquet"
+# new_preprocessor = HistoricalDataProcessor(task_new_data_path)
+# # Filter the data
+# training_data = processor.filtered_data()
+# future_data = new_preprocessor.filtered_data()
+
+df_ = preprocess_data(df_)
+df_ = df_[
+    (df_["PRODSOURCELABEL"].isin(["user", "managed"]))
+    & (df_["CTIME"] > .2)
+    & (df_["CTIME"] < 10000)
+    & (df_["RAMCOUNT"] < 8000)
+    & (df_["RAMCOUNT"] > 1)
+    & (df_["CPU_EFF"] > 10)
+    & (df_["CPU_EFF"] < 99)
+]
+
+training_data = df_.sample(frac=0.9, random_state=42)
+future_data = df_[
+    ~df_.index.isin(training_data.index)
+]  # Get the remaining rows
 
 #################################################################################################################
 #################################################################################################################
 # Prepare train test dataset for all the models in sequence RAMCOUNT-> cputime_HS -> CPU_EFF -> IOINTENSITY
 #################################################################################################################
+print(df_.shape)
+print(training_data.shape)
+print(future_data.shape)
 
 target_var = ["IOINTENSITY"]
 
-
-categorical_features = ["PRODSOURCELABEL", "P", "F", "CPUTIMEUNIT", "CORE"]
+categorical_features = ['PRODSOURCELABEL', 'P', 'F', 'CORE', 'CPUTIMEUNIT']
 encoder = CategoricalEncoder()
 category_list = encoder.get_unique_values(
     training_data, categorical_features
@@ -60,19 +161,7 @@ selected_columns = [
     "IOINTENSITY",
 ]
 
-# Further filter the training data based on specific criteria
-training_data = training_data[
-    (training_data["PRODSOURCELABEL"].isin(["user", "managed"]))
-    & (training_data["RAMCOUNT"] > 100)
-    & (training_data["RAMCOUNT"] < 6000)
-    & (training_data["CPU_EFF"] > 30)
-    & (training_data["CPU_EFF"] < 100)
-    & ((training_data["cputime_HS"] > 0.5) & (training_data["cputime_HS"] < 4))
-    | (
-        (training_data["cputime_HS"] > 10)
-        & (training_data["cputime_HS"] < 6000)
-    )
-]
+
 
 # label_mapping = {'low': 0, 'high': 1}
 #
@@ -108,7 +197,6 @@ numerical_features = [
     "cputime_HS",
     "CPU_EFF",
 ]
-categorical_features = ["PRODSOURCELABEL", "P", "F", "CPUTIMEUNIT", "CORE"]
 features = numerical_features + categorical_features
 
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@------------------
@@ -152,22 +240,21 @@ print(predictions.head())
 model_seq = "5"
 target_name = "io"
 # Define the storage path
-model_storage_path = f"ModelStorage/model{model_seq}/"
+model_storage_path = (
+    # Define the storage path
+    f"/data/model-data/ModelStorage/model{model_seq}/"
+)
 model_name = f"model{model_seq}_{target_name}"  # Define the model name
 # 'my_plots'  # Optional: specify a custom plots directory
-plot_directory_name = f"ModelStorage/plots/model{model_seq}"
-model_full_path = model_storage_path + model_name
-
+plot_directory_name = f"/data/model-data/ModelStorage/plots/model{model_seq}"
 
 joblib.dump(fitted_scalar, f"{model_storage_path}/scaler.pkl")
 
-
-# Save the model using ModelHandler
-# pipeline.ModelHandler.save_model(tuned_model, model_storage_path, model_name, format='keras')
-
+model_full_path = model_storage_path + model_name
 tuned_model.export(model_full_path)
 
 # Specifying custom column names when instantiating the class
+
 actual_column_name = (
     "IOINTENSITY"  # Change this to match your actual column name
 )
