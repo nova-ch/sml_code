0c21abc571524f3681c6e153f72c6fac63e46fe0
Tasnuva Chowdhury <tachowdh@cern.ch>
2025-02-10 19:16:19 -0700

    Update logger.py

--- src/scout_ml_package/utils/logger.py
+++ src/scout_ml_package/utils/logger.py
@@ -1,19 +1,26 @@
 # File: scout_ml_package/utils/logger.py
-import logging
-
-def configure_logger(logger_name, log_file_path, log_level=logging.DEBUG, console_level=logging.INFO):
+def configure_logger(logger_name, log_dir_path, log_file_name='app.log', log_level=logging.DEBUG, console_level=logging.INFO):
     """
     Configures a logger with specified name and log file path.
 
     Parameters:
     - logger_name: Name of the logger.
-    - log_file_path: Path to the log file.
+    - log_dir_path: Directory path for the log file.
+    - log_file_name: Name of the log file (default: 'app.log').
     - log_level: Logging level for the file handler (default: DEBUG).
     - console_level: Logging level for the console handler (default: INFO).
 
     Returns:
     - A configured logger object.
     """
+    import os
+
+    # Ensure the log directory exists
+    os.makedirs(log_dir_path, exist_ok=True)
+
+    # Construct the full log file path
+    log_file_path = os.path.join(log_dir_path, log_file_name)
+
     # Create a logger
     logger = logging.getLogger(logger_name)
     logger.setLevel(log_level)
@@ -38,5 +45,8 @@ def configure_logger(logger_name, log_file_path, log_level=logging.DEBUG, consol
     return logger
 
 # Usage
-#logger = configure_logger('my_logger', 'logs/my_app.log')
+# logger = configure_logger('demo_logger', '/data/model-data/logs', 'demo.log')
 
+# `log_dir_path`: Specifies the directory where the log file should be saved.
+# `log_file_name`: Specifies the name of the log file. If not provided, it defaults to `'app.log'`.
+# 	
